ab8e6183868262fac9841808d84764e2521fcd78
Henrique Souza <henrique.asouza@outlook.com>
2021-08-07 02:56:53 -0300

    Fonts and menu style

--- config.def.h
+++ config.def.h
@@ -2,12 +2,18 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gap pixel between windows */
+static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+
+static const char *fonts[]          = { "Hack:size=15:antialias=true:autohint=true", 
+										"monospace:size=12",
+										"Font Awesome 5 Free Solid:size=15:antialias=true:autohint=true",
+										"Font Awesome 5 Free Regular:size=15:antialias=true:autohint=true",
+										"Font Awesome 5 Brands:size=15:antialias=true:autohint=true" };
+
+static const char dmenufont[]       = "Hack:size=15:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +26,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,11 +66,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = 	{ "librewolf", NULL };
+static const char *web[] = 	{ "firefox", NULL };
 static const char *steam[] = 	{ "steam-native", NULL };
 static const char *vscode[] = 	{ "code", NULL };
 static const char *vbox[] = 	{ "virtualbox", NULL };
 static const char *filezilla[] = { "filezilla", NULL };
+static const char *spotify[] = { "spotify", NULL };
+static const char *discord[] = { "discord", NULL };
 
 
 
@@ -80,7 +89,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_z,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -108,6 +117,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_c, 	   spawn,          {.v = vscode } },
 	{ MODKEY|ShiftMask,             XK_v, 	   spawn,          {.v = vbox } },
 	{ MODKEY|ShiftMask,             XK_f, 	   spawn,          {.v = filezilla } },
+	{ MODKEY|ShiftMask,             XK_m, 	   spawn,          {.v = spotify } },
+	{ MODKEY|ShiftMask,             XK_d, 	   spawn,          {.v = discord } },
 	
 };
 
