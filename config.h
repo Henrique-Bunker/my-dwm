636041f8c28dfc643ffd3fcc0ae202ba41ffea0f
Henrique Souza <henrique.asouza@outlook.com>
2021-08-09 02:22:48 -0300

    Installed Title Color

--- config.h
+++ config.h
@@ -16,13 +16,13 @@ static const char *fonts[]          = { "Hack:size=12:antialias=true:autohint=tr
 										"Font Awesome 5 Brands:size=12:antialias=true:autohint=true" };
 
 /* Dracula theme colors */
-static const char norm_fg[] = "#bfbfbf";
+static const char norm_fg[] = "#bd93f9";
 static const char norm_bg[] = "#282a36";
-static const char norm_border[] = "#bfbfbf";
+static const char norm_border[] = "#bd93f9";
 
-static const char sel_fg[] = "#bd93f9";
+static const char sel_fg[] = "#bfbfbf";
 static const char sel_bg[] = "#282a36";
-static const char sel_border[] = "#bd93f9";
+static const char sel_border[] = "#bfbfbf";
 
 static const char urg_fg[] = "#f7f7fb";
 static const char urg_bg[] = "#5af78e";
@@ -46,7 +46,7 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
     [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
     //[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
-    //[SchemeTitle] = { title_fg,   title_bg,  norm_border },
+    [SchemeTitle] = { title_fg,   title_bg,  norm_border },
 };
 
 /* tagging */
